ce9b3e443f61a26311a680eb3a89c6094d4c59b6
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2020-06-14 16:03:44 +0530

    Installed Related Packages Microsoft.AspNetCore.ApiAuthorization.IdentityServer, Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore, Microsoft.AspNetCore.Identity.EntityFrameworkCore, MySql.Data.EntityFrameworkCore, MySql.Data.EntityFrameworkCore.Design, Microsoft.EntityFrameworkCore.Tools

---
+++ ReactInvoiceManagementApp2/InvoiceManagementApp2.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceManagementApp2.Infrastructure.Data
+{
+    public class ApplicationDbContext //: DbContext
+    {
+    }
+}
