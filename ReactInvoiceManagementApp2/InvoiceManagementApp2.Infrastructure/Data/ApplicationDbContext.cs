1b6492615e650efc3237b2679934d441e821acee
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2020-06-14 16:16:27 +0530

    Set DbContext.

--- ReactInvoiceManagementApp2/InvoiceManagementApp2.Infrastructure/Data/ApplicationDbContext.cs
+++ ReactInvoiceManagementApp2/InvoiceManagementApp2.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace InvoiceManagementApp2.Infrastructure.Data
 {
-    public class ApplicationDbContext //: DbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Books> Books { get; set; }
     }
 }
